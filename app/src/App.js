33ed6f9d5708d0181da3c3cc9dc7b3a14ae3b804
Aky Mac Donald <akymacd@gmail.com>
2022-09-06 12:58:20 -0400

    Added simple counter app

---
+++ app/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+
+
+function App() {
+
+  const [count, setCount] = useState(4);
+
+  const decrementCount = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
+  const incrementCount = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  return (
+
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
+
+  );
+}
+
+export default App;
